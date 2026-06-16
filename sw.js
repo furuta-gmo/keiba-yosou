d0b276bcd683ef98de5170be528bad69981cbe4c
Tomohisa Furuta <tomohisa-furuta@internet.gmo>
2026-06-16 13:06:48 +0900

    府中牝馬S・しらさぎS に新3視点(⑥調教・⑦スピード指数・⑧データ傾向)を追加=8視点に拡張 / SW v16

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 競馬予想PWA Service Worker
    バージョンを上げる(=CACHEを変える)と、次回オンライン時に更新が反映されます。 */
-const CACHE = 'keiba-yosou-v15';
+const CACHE = 'keiba-yosou-v16';
 
 const ASSETS = [
   './',
@@ -28,6 +28,9 @@ const ASSETS = [
   './2026-06-21_fuchu-himba/analysis/3_近走成績.html',
   './2026-06-21_fuchu-himba/analysis/4_コース馬場.html',
   './2026-06-21_fuchu-himba/analysis/5_オッズ分析.html',
+  './2026-06-21_fuchu-himba/analysis/6_調教.html',
+  './2026-06-21_fuchu-himba/analysis/7_指数.html',
+  './2026-06-21_fuchu-himba/analysis/8_データ傾向.html',
   './2026-06-21_shirasagi/prediction.html',
   './2026-06-21_shirasagi/research.html',
   './2026-06-21_shirasagi/odds.html',
@@ -36,7 +39,10 @@ const ASSETS = [
   './2026-06-21_shirasagi/analysis/2_血統.html',
   './2026-06-21_shirasagi/analysis/3_近走成績.html',
   './2026-06-21_shirasagi/analysis/4_コース馬場.html',
-  './2026-06-21_shirasagi/analysis/5_オッズ分析.html'
+  './2026-06-21_shirasagi/analysis/5_オッズ分析.html',
+  './2026-06-21_shirasagi/analysis/6_調教.html',
+  './2026-06-21_shirasagi/analysis/7_指数.html',
+  './2026-06-21_shirasagi/analysis/8_データ傾向.html'
 ];
 
 // インストール: アプリシェルを事前キャッシュ
